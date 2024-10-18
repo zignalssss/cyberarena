c87098c50ec0c9889c24aaab04bc9ba7ba1d84ac
zjeeee <wiritipon.t@ku.th>
2024-10-18 15:03:02 +0700

    first commit and make get card api

---
+++ src/app/api/card/getcard/route.js
@@ -0,0 +1,17 @@
+import clientPromise from '../../../../lib/mongo';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("CTFBOARD_DATABASE");
+    const data = await db.collection("CARD").find({}).toArray();
+    
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Unable to fetch data' }, { status: 500 });
+  }
+}
+
+// You can add other HTTP methods (POST, PUT, DELETE) as separate exported functions
